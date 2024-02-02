0c130e3f4cf23b49a99542b241bb41e77328c321
Ayush Bhardwaj <108116341+Bhardwaj-Ayush@users.noreply.github.com>
2024-02-02 10:15:20 +0530

    Add files via upload

---
+++ program3.pl
@@ -0,0 +1,11 @@
+ar :-
+    write('Enter length: '),
+    read(X),
+    write('Enter breadth: '),
+    read(Y),
+    A is X * Y,
+    write('Area of rectangle is: '),
+    write(A).
+
+
+
---
+++ sum.pl
@@ -0,0 +1,8 @@
+sum :-
+    write('Enter 1 '),
+    read(X),
+    write('Enter 2: '),
+    read(Y),
+    S is X + Y,
+    write('Sum is: '),
+    write(S).
